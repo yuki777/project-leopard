c4d55a9e03b2e972999946f0978754ac0bd55c6e
Yuki Adachi <yuki777@gmail.com>
2021-09-06 00:04:37 +0900

    Basic auth with l5-very-basic-auth on staging

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth.very_basic'], function() {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
