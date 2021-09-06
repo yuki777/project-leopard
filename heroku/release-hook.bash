b1dec759c58ecae1be3b77ca44305dc8b7a87392
Yuki Adachi <yuki777@gmail.com>
2021-09-07 06:02:33 +0900

    Improve release hook

--- heroku/release-hook.bash
+++ heroku/release-hook.bash
@@ -1,17 +1,29 @@
 #!/bin/bash -eu
 
-EMOJI=""
-if [ "$APP_ENV" = "production" ];then
-    EMOJI=":tada:"
-fi
-
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-if [ "$SLACK_NOTIFY_URL" ];then
-    curl -s -X POST --data-urlencode "payload={\"username\": \"Postdeploy Script\", \"text\": \"[$APP_ENV] Postdeploy script completed. $APP_URL $EMOJI \", \"icon_emoji\": \":rocket:\"}" $SLACK_NOTIFY_URL
+if [ "$SLACK_WEBHOOK_URL" ];then
+    EMOJI=""
+    if [ "$APP_ENV" = "production" ];then
+        EMOJI=":tada:"
+    fi
+
+    # Requirement: `heroku labs:enable runtime-dyno-metadata -a ...`
+    MESSAGE="{
+        \"blocks\": [
+            {
+                \"type\": \"section\",
+                \"text\": {
+                    \"type\": \"mrkdwn\",
+                    \"text\": \"*${APP_URL}* has been deployed to *${APP_ENV}* $EMOJI\nDeployed commit: <${GITHUB_URL}/commit/${HEROKU_SLUG_COMMIT}|${HEROKU_SLUG_COMMIT:0:7}>\"
+                }
+            }
+        ]
+    }"
+    curl -s -X POST -H "Content-type: application/json" --data "$MESSAGE" $SLACK_WEBHOOK_URL
 fi
 
 exit 0
